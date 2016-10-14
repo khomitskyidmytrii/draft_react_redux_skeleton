4b0de80aa3082976a0c06a07131bced2d5e2481c
khomitskyi <khomitskyi.dmytrii@gmail.com>
2016-10-15 00:46:54 +0200

    Basic Redux Flow

---
+++ src/actions/courseActions.js
@@ -0,0 +1,5 @@
+import * as actions from './actionTypes';
+
+export function createCourse(course) {
+  return { type: actions.CREATE_COURSE, course}
+}
---
+++ src/components/about/AboutPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class AboutPage extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>About</h1>
+        <p>Wat!? This app uses ES6?</p>
+      </div>
+    );
+  }
+}
+
+export default AboutPage;
