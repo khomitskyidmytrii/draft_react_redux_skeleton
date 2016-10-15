4bf166908a05aebdc8cb3d23faadbb23e57f92e8
khomitskyi <khomitskyi.dmytrii@gmail.com>
2016-10-15 23:37:05 +0200

    Async write

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -1,5 +1,39 @@
 import * as actions from './actionTypes';
+import courseApi from '../api/mockCourse';
 
-export function createCourse(course) {
-  return { type: actions.CREATE_COURSE, course}
+export function loadCoursesSuccess(courses) {
+  return { type: actions.LOAD_COURSES_SUCCESS, courses};
+}
+
+export function loadCourses() {
+  return function(dispatch) {
+    return courseApi.getAllCourses()
+      .then(courses => {
+        dispatch(loadCoursesSuccess(courses));
+      })
+      .catch(error => {
+        throw(error);
+      });
+  };
+}
+
+export function updateCoursesSuccess(course) {
+  return {type: actions.UPDATE_COURSE_SUCCESS, course};
+}
+
+export function createCourseSuccess(course) {
+  return {type: actions.CREATE_COURSE_SUCCESS, course};
+}
+
+export function saveCourse(course) {
+  return function(dispatch, getState) {
+    return courseApi.saveCourse(course)
+      .then(savedCourse => {
+        course.id ? dispatch(updateCoursesSuccess(savedCourse)) :
+          dispatch(createCourseSuccess(savedCourse));
+      })
+      .catch(err => {
+        throw(err);
+      });
+  };
 }
---
+++ src/reducers/authorReducers.js
@@ -0,0 +1,12 @@
+import * as actions from '../actions/actionTypes';
+import initialState from './initialState';
+
+export default function authorReducer(state = initialState.authors, action) {
+  switch(action.type) {
+    case actions.LOAD_AUTHORS_SUCCESS:
+      return action.authors;
+
+    default:
+      return state;
+  }
+}
